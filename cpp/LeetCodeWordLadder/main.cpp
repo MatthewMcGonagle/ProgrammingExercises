e17e4b8118606f74a0de07ec037b91ccde2d8ded
Matthew McGonagle <mwmcgonagle@gmail.com>
2017-04-27 16:42:31 -0700

    Fixed return length and return 0 when no ladder occurs. Solution accepted: faster than 16.8% of cpp submissions.

--- cpp/LeetCodeWordLadder/main.cpp
+++ cpp/LeetCodeWordLadder/main.cpp
@@ -5,14 +5,16 @@
 
 using namespace std;
 struct vertex {
+    vertex() { word = string(""); adjacents = vector<vertex*>(0); searchParent = NULL;}
     string word;
-    vector<vertex*> adjacents;
+    vector<vertex*> adjacents; 
 
     // Search parameters.
-    vertex* searchParent = NULL;
+    vertex* searchParent;
 };
 
 struct wordGraph {
+    wordGraph() {vertices = vector<vertex>(0);}
     vector<vertex> vertices; 
 };
 
@@ -23,13 +25,13 @@ class Solution {
         vertex* endv;
 }; 
  
-wordGraph makeGraph(vector<string>& wordList);
+void makeGraph(vector<string>& wordList, wordGraph& graph);
 void printGraph(wordGraph& g);
 void printParentChain(vertex* v);
 
 int main() {
-    string wordArray[] = {"hot", "dot", "dog", "lot", "log", "cog", "kog", "zot"};
-    vector<string> wordList(wordArray, wordArray + 8);
+    string wordArray[] = {"hot", "dot", "dog", "lot", "log", "cog"};
+    vector<string> wordList(wordArray, wordArray + 6);
     wordGraph g;
     Solution s;
     int chainlength;
@@ -40,9 +42,9 @@ int main() {
     cout << wordList[wordList.size()-1];
     cout << "}" << endl;
 
-    g = makeGraph(wordList);
+    makeGraph(wordList, g);
     printGraph(g);
-    chainlength = s.ladderLength(string("hot"), string("kog"), wordList);
+    chainlength = s.ladderLength(string("hit"), string("cog"), wordList);
     cout << "ladder length = " << chainlength << endl
          << "Ladder chain (backwards) is:" << endl;
     printParentChain(s.endv);
@@ -62,21 +64,23 @@ int stringDistance(string& x, string& y) {
     return numDifference;
 }
 
-wordGraph makeGraph(vector<string>& wordList) {
-    wordGraph graph;
-    vector<vertex> vertices(wordList.size()); 
+void makeGraph(vector<string>& wordList, wordGraph &graph) {
     vector<vertex>::iterator vertexIt, vertexJt;
     vector<string>::iterator wordIt, wordJt;
      
     // Initialize graph to so that every vertex is a word from wordList
+    graph.vertices = vector<vertex>(wordList.size());
 
-    for (wordIt = wordList.begin(), vertexIt = vertices.begin(); wordIt != wordList.end(); wordIt++, vertexIt++)
+    for (wordIt = wordList.begin(), vertexIt = graph.vertices.begin(); wordIt != wordList.end(); wordIt++, vertexIt++){
         vertexIt -> word = *wordIt;
-        vertexIt -> adjacents = vector<vertex*>(0);
+        // vertexIt -> adjacents = vector<vertex*>(0);
+        // vertexIt -> searchParent = NULL;
+    }
 
     // Loop through to find adjacencies (which is when word distance == 1).
 
-    for( wordIt = wordList.begin(), vertexIt = vertices.begin(); wordIt != wordList.end(); wordIt++, vertexIt++) {
+    for( wordIt = wordList.begin(), vertexIt = graph.vertices.begin()
+       ; wordIt != wordList.end(); wordIt++, vertexIt++) {
         for( wordJt = wordIt + 1, vertexJt = vertexIt + 1; wordJt != wordList.end(); wordJt++, vertexJt++) {
             if(stringDistance(*wordIt, *wordJt) == 1) {
                 vertexIt -> adjacents.push_back(& *vertexJt);
@@ -84,9 +88,6 @@ wordGraph makeGraph(vector<string>& wordList) {
             }
         }
     } 
-
-    graph.vertices = vertices;
-    return graph;
 
 }
 
@@ -115,46 +116,64 @@ void printParentChain(vertex* v) {
 int Solution::ladderLength(string beginWord, string endWord, vector<string>& wordList) {
     int length;
     bool found;
-    vector<vertex>::iterator startIt, vertexIt;
+    vector<vertex>::iterator vertexIt;
     vector<vertex*>::iterator adjIt;
+    vector<string>::iterator wordIt;
     queue<vertex*> toProcess;
-    vertex* current;
-    g = makeGraph(wordList);
+    vertex *current, *startv;
+
+    // See if beginWord needs to be added to the wordList
+    found = false;
+    for(wordIt = wordList.begin(); wordIt != wordList.end() && !found; wordIt++)
+        if(*wordIt == beginWord) 
+            found = true;
+    if(!found)
+        wordList.push_back(beginWord);
+    
+    makeGraph(wordList, g);
     
     // Find position of beginWord in vertex list. Also initialize the search parents to be
     // NULL.
     for( vertexIt = g.vertices.begin(); vertexIt != g.vertices.end(); vertexIt++) {
         if(vertexIt -> word == beginWord) 
-            startIt = vertexIt;
+            startv = & (*vertexIt);
         vertexIt -> searchParent = NULL; 
     }
     
     // Do a breadth first search. Start with *startIt.
-    toProcess.push(& (*startIt));
+    toProcess.push(startv);
     found = false;
     
     while(!toProcess.empty() && !found) {
         current = toProcess.front();
         toProcess.pop();
+        // cout << "After popping, toProcess size = " << toProcess.size() << endl;
 
         // Check if current is the desired endWord. Else process adjacents of current vertex.
         if(current -> word == endWord) 
             found = true;
         else {
-            for(adjIt = current -> adjacents.begin(); adjIt != current -> adjacents.end(); adjIt++)
-                if( (*adjIt) != & (*startIt) && (*adjIt) -> searchParent == NULL) {
+            //cout << "Number adjacents for current = " << current -> word  << " is " << current->adjacents.size() << endl;
+            for(adjIt = current -> adjacents.begin(); adjIt != current -> adjacents.end(); adjIt++) {
+                // cout << "Got a NULL" << endl;
+                if( (*adjIt) != startv && (*adjIt) -> searchParent == NULL) {
                     (*adjIt) -> searchParent = current;
                     toProcess.push(*adjIt);
                 }
+            }
         }
 
     }
+
+    // Check if not found. In this case, return 0.
+    if(!found)
+        return 0;
 
     // endWord should be a current. Problem assumes endWord does occur in graph. So the above loop must have
     // terminated by finding endWorld. Now backtrack along parents.
 
     endv = current;
-    length = 0;
+    length = 1;
     while(current -> searchParent != NULL ) {
         length++;
         current = current -> searchParent;
