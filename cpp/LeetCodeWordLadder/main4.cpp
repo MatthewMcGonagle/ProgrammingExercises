a2c46e7d2e60f1d9d0dc6b311df9c924063747e9
Matthew McGonagle <mwmcgonagle@gmail.com>
2017-05-03 18:11:38 -0700

    Finished bi-directional breadth first search for shortest path through the word graph. Solution is in main4.cpp. Solution accepted as being faster than 18.16% of all cpp submissions.

--- cpp/LeetCodeWordLadder/main4.cpp
+++ cpp/LeetCodeWordLadder/main4.cpp
@@ -13,19 +13,20 @@ struct nodeInfo {
 class Solution {
     public:
         int ladderLength(string beginWord, string endWord, vector<string>& wordList);
-        int distance(string* x, string* y);
+        int stringDistance(string* x, string* y);
 
     private:
 
         void possiblyAddToGraph(nodeInfo* inGraph, nodeInfo* target);
+        bool addWhenClose(nodeInfo* inGraph, nodeInfo* target, nodeInfo **parent);
         int traceDistance();
         queue<nodeInfo*> toProcess;
-        bool found;
         nodeInfo* midpoint;
 };
 
 int main() {
     string wordArray[] = {"hot", "dot", "dog", "lot", "log", "cog"};
+    //string wordArray[] = {"a", "b", "c"};
     vector<string> wordList(wordArray, wordArray + 6);
     Solution s;
     int chainlength;
@@ -37,6 +38,7 @@ int main() {
     cout << "}" << endl;
 
     chainlength = s.ladderLength(string("hit"), string("cog"), wordList);
+    //chainlength = s.ladderLength(string("a"), string("c"), wordList);
     cout << "ladder length = " << chainlength << endl;
 
     return 0;
@@ -44,83 +46,112 @@ int main() {
 
 int Solution::ladderLength(string beginWord, string endWord, vector<string>& wordList) {
 
-    int beginDistance, endDistance, stringDistance;
+    int stringDistance;
     vector<nodeInfo> nodeList(wordList.size());
     vector<nodeInfo>::iterator nodeIt;
     vector<string>::iterator wordIt;
-    nodeInfo swapper, begin, end, dummy, *current;
+    nodeInfo swapper, begin, dummy, *current;
+    bool foundBegin, foundEnd;
 
-    // Initialize node info list.
+    // Initialize node info list. Search for beginWord and endWord in the list. When encountering either, 
+    // add it to the processing queue.
 
+    toProcess = queue<nodeInfo*>();
+    foundBegin = false;
+    foundEnd = false;
     for(nodeIt = nodeList.begin(), wordIt = wordList.begin(); nodeIt != nodeList.end(); nodeIt++, wordIt++) {
         nodeIt -> word = &*wordIt; 
         nodeIt -> beginParent = NULL;
         nodeIt -> endParent = NULL;
+
+        if(*wordIt == endWord) {
+            toProcess.push(&*nodeIt);
+            nodeIt -> endParent = &dummy;
+            foundEnd = true;        
+        }
+        else if(*wordIt == beginWord) {
+            toProcess.push(&*nodeIt);
+            nodeIt -> beginParent = &dummy;
+            foundBegin = true;
+        }
+        
     }
 
-    // Set up begin info and end info. For processing purposes, begin info and end info point to a dummy node.
-    begin.word = &beginWord;
-    begin.beginParent = &dummy;
-    begin.endParent = NULL;
+    // If endWord wasn't found in list then return failure with length 0.
 
-    end.word = &endWord;
-    end.beginParent = NULL;
-    end.endParent = &dummy; 
+    if(!foundEnd)
+        return 0;
+
+    // If beginning word wasn't found, then it isn't in wordList. This is okay. We 
+    // just need to set up an external node for it. 
+    // Also set up dummy node pointed to by beginning and end nodes.
+    
+    if(!foundBegin) {
+        begin.word = &beginWord;
+        begin.beginParent = &dummy;
+        begin.endParent = NULL;
+
+        toProcess.push(&begin);
+    }
 
     dummy.word = NULL;
     dummy.beginParent = NULL;
     dummy.endParent = NULL;
     
-    // First put beginWord and endWord into processing queue.
-    toProcess = queue<nodeInfo*>();
-    toProcess.push(&begin);
-    toProcess.push(&end);
-
     // Now start processing until empty or found.
 
-    found = false;
-    beginDistance = 0;
-    endDistance = 0;
     midpoint = NULL;
-    while(!toProcess.empty() && !found) {
+    while(!toProcess.empty() && midpoint == NULL) {
 
         current = toProcess.front();
         toProcess.pop();
 
-        for(nodeIt = nodeList.begin(); nodeIt != nodeList.end(); nodeIt++) 
+        for(nodeIt = nodeList.begin(); nodeIt != nodeList.end() && midpoint == NULL; nodeIt++) 
            possiblyAddToGraph(current, &*nodeIt); 
     }
 
     if( midpoint == NULL)
         return 0;
     else
-        return traceDistance();
+        // Number of vertices is one more than the length from start to end.
+        return 1 + traceDistance();
         
 }
 
 void Solution::possiblyAddToGraph(nodeInfo *inGraph, nodeInfo *target) {
-    nodeInfo **inGraphParent, **foundParent;
-    int *distanceToUpdate;
+
+    bool targetAdded;
+
     if(inGraph -> beginParent != NULL && target -> beginParent == NULL) {
-        inGraphParent = & (target -> beginParent);
-        foundParent = & (target -> endParent);
+
+        targetAdded = addWhenClose(inGraph, target, &(target -> beginParent));
+        if( targetAdded ) { 
+            if(target -> endParent != NULL) {
+                midpoint = target;
+            }         
+        } 
     }
+
     else if (inGraph -> endParent != NULL && target -> endParent == NULL) {
-        inGraphParent = & (target -> endParent);
-        foundParent = & (target -> beginParent);
+        targetAdded = addWhenClose(inGraph, target, &(target -> endParent));
+        if ( targetAdded) {
+            if(target -> beginParent != NULL) {
+                midpoint = target;
+            }
+        } 
+        
+    }
+
+}
+
+bool Solution::addWhenClose(nodeInfo *inGraph, nodeInfo *target, nodeInfo **targetParent) {
+    if(stringDistance(inGraph -> word, target -> word) == 1) {
+        toProcess.push(target);
+        *targetParent = inGraph;
+        return true;
     }
     else
-        return;
-
-    if(distance( inGraph -> word, target -> word)) {
-        *inGraphParent = inGraph;
-        toProcess.push(target);
-        if(*foundParent != NULL) {
-            found = true;
-            midpoint = target;
-        }
-    }
-
+        return false;
 }
 
 int Solution::traceDistance() {
@@ -135,14 +166,18 @@ int Solution::traceDistance() {
 
     current = midpoint;
     while(current != NULL) {
-        current = current -> endParent;
-        distance++;
+       current = current -> endParent;
+       distance++;
     }
 
-    return distance - 2; // Will count dummy edges that we artificially added.
+    // Will count dummy edges that we artificially added. We also count two edges for both times we
+    // we move from the dummy to its NULL parent (once for beginnning and once for end). So we subtract 
+    // four total.
+
+    return distance - 4; 
 }
 
-int Solution::distance(string* x, string* y) {
+int Solution::stringDistance(string* x, string* y) {
 
     int numdifference = 0;
     string::iterator xIt, yIt;
