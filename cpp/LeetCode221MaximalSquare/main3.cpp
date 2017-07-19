61b96b1dac9ac42e952d364f0a07e0db37764dad
Matthew McGonagle <mwmcgonagle@gmail.com>
2017-07-18 19:00:53 -0700

    Fixed bad behavior by using reverse_iterator instead of regular iterators. Solution passes as beating 57.43% of all cpp submissions.

--- cpp/LeetCode221MaximalSquare/main3.cpp
+++ cpp/LeetCode221MaximalSquare/main3.cpp
@@ -17,9 +17,9 @@ class Solution {
     private:
         void computeLengthMatrix(vector<vector<char> > &matrix, vector<vector<int> > &lengths);
         template <typename T>
-        void initPosIt(typename vector<T>::iterator posIt[2][2], vector<T> &row1, vector<T> &row2);
+        void initPosIt(typename vector<T>::reverse_iterator posIt[2][2], vector<T> &row1, vector<T> &row2);
         template <typename T>
-        void decrPosIt(typename vector<T>::iterator posIt[2][2]);
+        void moveLeft(typename vector<T>::reverse_iterator posIt[2][2]);
         int updateLength(int a, int b, int c);
 
         int maxLength;
@@ -34,13 +34,20 @@ template void printMatrix<int>(vector<vector<int> > &matrix);
 int main() {
 
     int maxSize;
+    const int m = 7, n = 1;
+    char strings[m][n+1] = {"1", "0", "1", "1", "1", "1", "0"};
+    // const int m = 4, n = 5;
+    // char strings[m][n+1] = {"10100",
+    //                         "10111",
+    //                         "11111",
+    //                         "10010"};
     // This case failed on Leetcode as giving 1 as the area, but
     // it seems to return 0 on my machine.
-    const int m = 4, n = 3;
-    char strings[m][n+1] = {"000",
-                            "000",
-                            "000",
-                            "000"};
+    // const int m = 4, n = 3;
+    // char strings[m][n+1] = {"000",
+    //                         "000",
+    //                         "000",
+    //                         "000"};
 
     // const int m = 4, n = 5;
     // char strings[n][n+1] = {"00000",
@@ -95,10 +102,10 @@ int Solution::maximalSquare(vector<vector<char> > &matrix) {
 
 void Solution::computeLengthMatrix(vector<vector<char> > &matrix, vector<vector<int> > &lengths) {
 
-    vector<vector<int> >::iterator rowIt;
-    vector<vector<char> >::iterator rowIt2;
-    vector<int>::iterator posIt[2][2];
-    vector<char>::iterator posIt2;
+    vector<vector<int> >::reverse_iterator rowIt;
+    vector<vector<char> >::reverse_iterator rowIt2;
+    vector<int>::reverse_iterator posIt[2][2];
+    vector<char>::reverse_iterator posIt2;
 
     // Iterate over comparing mimima using position iterators. posIt[][] represents the following
     // posIt[0][0]  posIt[0][1]
@@ -107,10 +114,10 @@ void Solution::computeLengthMatrix(vector<vector<char> > &matrix, vector<vector<
     maxLength = 0;
 
     // Last row of lengths is just the last row of matrix, converted to integers.
-    rowIt = lengths.end() - 1;
-    rowIt2 = matrix.end() - 1; 
-    for(posIt[0][0] = rowIt -> begin(), posIt2 = rowIt2 -> begin();
-        posIt[0][0] != rowIt -> end();
+    rowIt = lengths.rbegin();
+    rowIt2 = matrix.rbegin(); 
+    for(posIt[0][0] = rowIt -> rbegin(), posIt2 = rowIt2 -> rbegin();
+        posIt[0][0] != rowIt -> rend();
         posIt[0][0]++, posIt2++) {
 
 
@@ -121,12 +128,12 @@ void Solution::computeLengthMatrix(vector<vector<char> > &matrix, vector<vector<
   
     // Last column of lengths is also the same as that of matrix, just converted to integers.
 
-    for(rowIt = lengths.begin(), rowIt2 = matrix.begin();
-        rowIt != lengths.end();
+    for(rowIt = lengths.rbegin(), rowIt2 = matrix.rbegin();
+        rowIt != lengths.rend();
         rowIt++, rowIt2++) {
 
-        *(rowIt -> end() - 1) = *(rowIt2 -> end() - 1) - '0'; 
-        if (*posIt[0][0] == 1)
+        *(rowIt -> rbegin()) = *(rowIt2 -> rbegin()) - '0'; 
+        if (*(rowIt -> rbegin()) == 1)
             maxLength = 1;
     } 
 
@@ -134,12 +141,12 @@ void Solution::computeLengthMatrix(vector<vector<char> > &matrix, vector<vector<
     if(matrix.size() == 1 || matrix.begin() -> size() == 1)
         return;
 
-    // Now iterate to the left and up over the remaining rows.
+    // Now iterate to the left and up over the remaining rows. Make sure to use reverse iterators. 
 
-    for(rowIt = lengths.end() - 2, rowIt2 = matrix.end() - 2; rowIt != lengths.begin() - 1; rowIt--, rowIt2--) {
-        for ( initPosIt(posIt, *rowIt, *(rowIt + 1)), posIt2 = rowIt2 -> end() - 2;
-              posIt[0][0] != rowIt -> begin() - 1;
-              decrPosIt<int>(posIt), posIt2--) {
+    for(rowIt = lengths.rbegin() + 1, rowIt2 = matrix.rbegin() + 1; rowIt != lengths.rend(); rowIt++, rowIt2++) {
+        for ( initPosIt(posIt, *rowIt, *(rowIt - 1)), posIt2 = rowIt2 -> rbegin() + 1;
+              posIt[0][0] != rowIt -> rend() ;
+              moveLeft<int>(posIt), posIt2++) {
 
             if(*posIt2 == '1') {
                 *posIt[0][0] = updateLength(*posIt[0][1], *posIt[1][1], *posIt[1][0]);
@@ -152,23 +159,23 @@ void Solution::computeLengthMatrix(vector<vector<char> > &matrix, vector<vector<
 }
 
 template <typename T>
-void Solution::initPosIt(typename vector<T>::iterator posIt[2][2], vector<T> &row1, vector<T> &row2) {
+void Solution::initPosIt(typename vector<T>::reverse_iterator posIt[2][2], vector<T> &row1, vector<T> &row2) {
     
-    posIt[0][1] = row1 . end() - 1;
-    posIt[0][0] = posIt[0][1] - 1;
+    posIt[0][1] = row1 . rbegin();
+    posIt[0][0] = posIt[0][1] + 1;
    
-    posIt[1][1] = row2 . end() - 1;
-    posIt[1][0] = posIt[1][1] - 1; 
+    posIt[1][1] = row2 . rbegin();
+    posIt[1][0] = posIt[1][1] + 1; 
 
 }
 
 template <typename T>
-void Solution::decrPosIt(typename vector<T>::iterator posIt[2][2]) {
+void Solution::moveLeft(typename vector<T>::reverse_iterator posIt[2][2]) {
 
-    posIt[0][0]--;
-    posIt[0][1]--;
-    posIt[1][0]--;
-    posIt[1][1]--;
+    posIt[0][0]++;
+    posIt[0][1]++;
+    posIt[1][0]++;
+    posIt[1][1]++;
 
 }
 
