d7d6ee40398b9b8d972fa1c4156f07767deac59a
Matthew McGonagle <mwmcgonagle@gmail.com>
2018-08-08 16:28:09 -0400

    Implemented solution 3 which uses Rational representations of coefficients. Eliminates need for computing greatest common divisors, but runs much slower than other solution.

--- cpp/LeetCode149MaxPointOnLine/solution3.cpp
+++ cpp/LeetCode149MaxPointOnLine/solution3.cpp
@@ -11,7 +11,9 @@
     represent 7/5 = 1 + 2/5. So we only need to do a single division and modulo operation. No need to find
     gcd's. Then to compare, we just use convert to long and do cross-multiplication.
 
-    Solution Passes as being faster than 62.40% of all accepted cpp solutions (8 ms).
+    Solution Passes as being faster than 18.24% of all accepted cpp solutions (16 ms). So now real improvement
+    over solution 2. Also, the use of long to keep track of cross-multiplication seems a little suspect as
+    the exact size of int vs long is compiler dependent.
 **/
 
 #include<iostream>
@@ -79,11 +81,30 @@ struct Rational {
     int intPart, num, denom;
 
     Rational() : intPart(0), num(0), denom(1) {}
+    Rational(int n) : intPart(n), num(0), denom(1) {}
     Rational(int num_, int denom_);
+
+    inline bool nonZero() {return !(intPart == 0 && num == 0); }
 
     friend ostream& operator<<(ostream& os, const Rational& rhs);
     friend bool operator<(const Rational& lhs, const Rational& rhs);
+    friend bool operator>(const Rational& lhs, const Rational& rhs){ return rhs < lhs; }
 };
+
+// /**
+//     Can compute the product of two integers with precise precision. Use this for accurately
+//     doing cross-multiplication.
+// 
+// **/
+// 
+// struct LargeInt {
+// 
+//     int mostSig, leastSig;
+//     LargeInt() : mostSig(0), leastSig(0) {}
+//     LargeInt(int n) : mostSig(0), leastSig(n) {}
+// 
+//     void product(int x, int y);
+// };
 
 /**
 
@@ -100,20 +121,18 @@ struct Rational {
 
 struct Line {
 
-    int coeff[3];
+    Rational coeff[3];
 
     Line(const Point& a, const Point& b);
-    Line() { 
-        for(int i = 0; i < 3; i++)
-            coeff[i] = 0;
 
+    Line() { 
+        
+        for(int i = 0; i < 3; i++)
+            coeff[i] = Rational(0);
     }
 
     private:
-    inline void getInitCoeffs(const Point &a, const Point &b);
-    inline void removeGCD();
-    inline void enforceSignRule();
-    inline void inverseCoeff();
+    inline void enforceUniqueness(const int initCoeff[3]);
 
     friend bool operator<(const Line& lhs, const Line& rhs);
 };
@@ -125,12 +144,7 @@ struct Line {
 
 ostream& operator<<(ostream& os, const Point& p);
 ostream& operator<<(ostream& os, const MultiPoint& mp); 
-
-/**
-    Greatest common divisor function.
-**/
-
-int gcd(int nums[], int size);
+ostream& operator<<(ostream& os, const Line& l);
 
 /**
     The solution class. Leetcode specifies the form of the member function maxPoints. 
@@ -142,6 +156,7 @@ class Solution {
     public:
 
     int maxPoints(vector<Point>& points);
+    void printMap();
 
     private:
 
@@ -162,7 +177,17 @@ class Solution {
 
 int main() {
 
-    int coordsArray[][2] = {{1,1}, {2,2}, {3,3}, {2, 2}};
+    //int coordsArray[][2] = {{1,1}, {2,2}, {3,3}, {2, 2}};
+
+    // Input [[1,1],[3,2],[5,3],[4,1],[2,3],[1,4]] fails for division by 0.
+    // int coordsArray[][2] = {{1,1}, {3,2}, {5,3}, {4,1}, {2,3}, {1,4}};
+    // Input [[0,0],[65536,65536],[65536,131072]] failed with output 3, expected 2.
+    //int coordsArray[][2] = {{0, 0}, {65536, 65536}, {65536, 131072}};
+    // Input [[0,-12],[5,2],[2,5],[0,-5],[1,5],[2,-2],[5,-4],[3,4],[-2,4],[-1,4],[0,-5],[0,-8],[-2,-1],[0,-11],[0,-9]]
+    // failes with output 3, expected 6.
+    //int coordsArray[][2] = {{0,-12},{5,2},{2,5},{0,-5},{1,5},{2,-2},{5,-4},{3,4},{-2,4},{-1,4},{0,-5},{0,-8},{-2,-1},{0,-11},{0,-9}};
+    // Input [[0,0],[94911151,94911150],[94911152,94911151]] fails with output 3, expected 2.
+    int coordsArray[][2] = {{0,0},{94911151,94911150},{94911152,94911151}};
     vector<int[2]> coords(coordsArray, coordsArray + sizeof(coordsArray) / sizeof(int) / 2);
     vector<Point> points;
     vector<MultiPoint> mPoints;
@@ -171,13 +196,27 @@ int main() {
     Rational x, y;
 
     cout << "RATIONAL NUMBER TEST" << endl
-        << "Rational(-5, 3) = " << Rational(-5, 3) << endl 
-        << "Rational(9, 4) = " << Rational(10, 4) << endl;
+        << "Rational(-10, 4) = " << Rational(-10, 4) << endl 
+        << "Rational(10, -4) = " << Rational(10, -4) << endl
+        << "Rational(10, 4) = " << Rational(10, 4) << endl
+        << "Rational(-10, -4) = " << Rational(-10, -4) << endl;
 
     x = Rational(10, 4);
     y = Rational(18, 7);
 
     cout << "( " << x << " < " << y << " ) = " << (x < y) << endl << endl;
+
+    x = Rational(3,2);
+    y = Rational(6,4);
+    cout << "( " << x << " < " << y << " ) = " << (x < y) << endl << endl;
+    cout << "( " << x << " > " << y << " ) = " << (x > y) << endl << endl;
+
+    x = Rational(3,2);
+    y = Rational(-10, 3);
+    cout << "( " << x << " > " << y << " ) = " << (x > y) << endl << endl;
+
+    
+
 
     cout << "POINTS AND MULTIPOINTS TESTS" << endl
          << "Constructing Points and MultiPoints" << endl;
@@ -191,20 +230,29 @@ int main() {
     }
 
     // Let's print out the points. No need to be efficient here. 
+
     for(int i = 0; i < points.size(); i++)
         cout << points[i] << "\t"; 
     cout << endl;
 
     // Let's print out the multi-points.
+
     for(int i = 0; i< mPoints.size(); i++)
         cout << mPoints[i] << "\t";
     cout << endl;
+
+    // Let's try constructing some lines.
+
+    cout << "The line through " << points[0] << " and " << points[1] << " is " << Line(points[0], points[1]) << endl;
 
     // Now let's test the Solution.
 
     cout << "Finding the maximum number of points" << endl;
     maxPoints = solver.maxPoints(points);
     cout << "The maximum number of points = " << maxPoints << endl;
+
+    solver.printMap();
+
     return 0;
 
 }
@@ -220,18 +268,22 @@ bool operator<(const Point& lhs, const Point& rhs) {
 
 }
 
-Rational::Rational(int num_, int denom_) {
+Rational::Rational(const int num_, const int denom_) {
 
     if(denom_ < 0) {
-        num_ *= -1;
-        denom_ *= -1;
+        num = num_ * -1;
+        denom = denom_ * -1;
     }
+    else {
+        num = num_;
+        denom = denom_;
+    } 
 
-    intPart = num_ / denom_;
-    num = num_ % denom_;
-    denom = denom_; 
+    intPart = num / denom;
+    num = num % denom;
+
     if(num < 0) {
-        num += denom_;
+        num += denom;
         intPart -= 1;
     }
 
@@ -239,11 +291,15 @@ Rational::Rational(int num_, int denom_) {
 
 ostream& operator<<(ostream& os, const Rational& rhs) {
 
-    os << rhs.intPart << " + " << rhs.num << " / " << rhs.denom;
+    if (rhs.intPart != 0)
+        os << rhs.intPart << " + ";
+    os << rhs.num << " / " << rhs.denom;
     return os;
 }
 
 bool operator<(const Rational& lhs, const Rational& rhs) {
+
+    long check[2];
 
     // First check the integer part as it is most significant.
 
@@ -253,57 +309,43 @@ bool operator<(const Rational& lhs, const Rational& rhs) {
     // Now we need to make the comparison at the fractional level using
     // cross multiplication.
 
-    return lhs.num * rhs.denom < rhs.num * lhs.denom; 
+    check[0] = lhs.num;
+    check[0] *= rhs.denom;
+    check[1] = rhs.num;
+    check[1] *= lhs.denom;
+    return check[0] < check[1];
 }
 
 Line::Line(const Point& a, const Point& b) {
 
-    getInitCoeffs(a, b);
-    removeGCD();
-    enforceSignRule();
+    int initCoeff[3];
+    initCoeff[0] = b.y - a.y;
+    initCoeff[1] = a.x - b.x;
+    initCoeff[2] = -initCoeff[0] * a.x - initCoeff[1] * a.y;
+
+    enforceUniqueness(initCoeff);
 
 }
 
-void Line::getInitCoeffs(const Point &a, const Point &b) {
+void Line::enforceUniqueness(const int initCoeff[3]) {
 
-    // coeff[0] and coeff[1] come from a choice of (un-normalized) normal direction to the line.
-    // coeff[0] = change in y.
-    coeff[0] = b.y - a.y; 
+    // Break into cases of whether the x coefficient is non-vanishing.
 
-    // coeff[1] = - change in x. 
-    coeff[1] = a.x - b.x;
+    if(initCoeff[0] != 0) {
 
-    // coeff[2] is found by plugging in one of the points.
+        coeff[0] = Rational(1);
+       
+        for(int i = 1; i < 3; i++)
+            coeff[i] = Rational(initCoeff[i], initCoeff[0]);
+    }
+    else {
 
-    coeff[2] = - coeff[0] * a.x - coeff[1] * a.y;
-
-
+        coeff[0] = Rational(0);
+        coeff[1] = Rational(1);
+        coeff[2] = Rational(initCoeff[2], initCoeff[1]);
+    }
 }
 
-void Line::removeGCD() {
-
-    int coeffGCD = gcd(coeff, 3);
-    
-    for(int i = 0; i < 3; i++)
-        coeff[i] /= coeffGCD; 
-
-}
-
-void Line::enforceSignRule() {
-
-    if (coeff[0] < 0)
-        inverseCoeff();
-    else if(coeff[0] == 0 && coeff[1] < 0)
-        inverseCoeff();
-
-}
-
-void Line::inverseCoeff() {
-
-    for(int i = 0; i < 3; i++)
-        coeff[i] *= -1;
-
-}
 
 bool operator<(const Line& lhs, const Line& rhs) {
 
@@ -335,13 +377,12 @@ ostream& operator<<(ostream& os, const MultiPoint& mp) {
    return os;
 }
 
-int gcd(int nums[], int size) {
+ostream& operator<<(ostream& os, const Line& l) {
 
-    int pairwiseGcd = nums[0];
-    for(int i = 1; i < size; i++)
-        pairwiseGcd = __gcd(pairwiseGcd, nums[i]);
+    os << "( " << l.coeff[0] << " ) x + ( " << l.coeff[1] << " ) y + "
+       << l.coeff[2] << " = 0";    
 
-    return pairwiseGcd;
+    return os;
 }
 
 int Solution::maxPoints(vector<Point>& points) {
@@ -364,14 +405,16 @@ int Solution::maxPoints(vector<Point>& points) {
     pointsOnLine.clear();
 
     for (mp[0] = mPoints.begin(); mp[0] != mPoints.end() - 1; mp[0]++) 
-
         doSweep(*mp[0], mp[0] + 1, mPoints.end());
 
-    for(int i = 0; i<mPoints.size(); i++)
-        cout << mPoints[i] << "\t";
-    cout << endl;
-
     return maxCount;
+}
+
+void Solution::printMap() {
+
+    map<Line, int>::const_iterator mapIt;
+    for(mapIt = pointsOnLine.begin(); mapIt != pointsOnLine.end(); mapIt++)
+        cout << mapIt -> first << " has " << mapIt -> second << " points" << endl;
 }
 
 vector<MultiPoint> Solution::aggregatePoints(const vector<Point>& points) {
@@ -421,21 +464,18 @@ int Solution::trivialCount(vector<MultiPoint>& mPoints) {
 
 void Solution::doSweep(const MultiPoint& commonEndPoint, vector<MultiPoint>::const_iterator begin, vector<MultiPoint>::const_iterator end) {
 
-    Point* p[2];
     vector<MultiPoint>::const_iterator mp;
     map<Line, int> sweepCounts;
     map<Line, int>::iterator lineCount;
     Line line;
 
-    p[0] = (Point*) &commonEndPoint;
-
     for(mp = begin; mp != end; mp++) {
 
-        p[1] = (Point*) &(*mp);
-        line = Line(*p[0], *p[1]); 
+        line = Line(commonEndPoint, *mp);
         updateSweepCount(line, *mp, sweepCounts);
 
     }
+
 
     // For all of the lines we found during the sweep, we never counted the common end point. So we now have
     // to add this multiplicity in. Then insert into our master map of line counts. Also update the maximum
@@ -443,12 +483,14 @@ void Solution::doSweep(const MultiPoint& commonEndPoint, vector<MultiPoint>::con
 
     for(lineCount = sweepCounts.begin(); lineCount != sweepCounts.end(); lineCount++) {
 
-        lineCount -> second += commonEndPoint.count; 
+        (lineCount -> second) += commonEndPoint.count; 
         pointsOnLine.insert(*lineCount);
 
-        if(lineCount -> second > maxCount)
+        if(lineCount -> second > maxCount) 
             maxCount = lineCount -> second;
     }
+
+
 }
 
 void Solution::updateSweepCount(const Line& line, const MultiPoint& newPoint, map<Line, int>& sweepCount) {
